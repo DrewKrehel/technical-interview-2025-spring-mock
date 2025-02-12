1e8489e73dee35a99babdc7e5489f1e8c4ef1aa8
Drew Krehel <drewkrehel@gmail.com>
2025-02-11 21:07:09 -0600

    try 1

--- coffee_shop.rb
+++ coffee_shop.rb
@@ -7,7 +7,7 @@ class CoffeeShop
     @inventory = Inventory.new
   end
 
-  def self.lace_order(type, size)
+  def place_order(type, size)
     coffee = Coffee.new(type, size) 
     puts "📖 Placed order for a #{coffee.description}"  
     @orders.push(coffee)
@@ -16,8 +16,8 @@ class CoffeeShop
   def fulfill_orders
     puts "🔄 Fulfilling orders..."
 
-    orders.each do |coffee|
-      if !@inventory.has_enough_supplies?(1)
+    @orders.each do |coffee|
+      if @inventory.has_enough_supplies?(1)
         @inventory.use_supplies(1)
         puts "✅ Fulfilled order for #{coffee.description}"
       else
@@ -25,12 +25,13 @@ class CoffeeShop
       end
     end
 
-    orders.clean_up
+    @orders.clear
+    clean_up
   end
 
   private
 
   def clean_up
-    puts "There are #{@orders.count} orders remaining!"
+    puts "🏁 There are #{@orders.count} orders remaining!"
   end
 end
---
+++ test1.rb
@@ -0,0 +1,9 @@
+☕ Welcome to Java Haven!
+📖 Placed order for a Medium Latte
+📖 Placed order for a Large Cappuccino
+📖 Placed order for a Small Espresso
+🔄 Fulfilling orders...
+✅ Fulfilled order for Medium Latte
+✅ Fulfilled order for Large Cappuccino
+❌ Insufficient supplies for Small Espresso
+🏁 There are 0 orders remaining!
