ad5487f4a61c7304d7c96196f9f850bb6fa1ff13
Drew Krehel <drewkrehel@gmail.com>
2025-06-19 18:37:36 +0000

    060925 done

--- simulation.rb
+++ simulation.rb
@@ -5,5 +5,7 @@ shop = CoffeeShop.new
 puts "☕ Welcome to Java Haven!"
 
 shop.place_order("latte", "medium")
+shop.place_order("cappuccino", "large")
+shop.place_order("espresso", "small")
 
 shop.fulfill_orders
